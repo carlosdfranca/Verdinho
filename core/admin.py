3eba46d0085f634b6be1257241ec6eaf031294cb
Carlos França <carlosdudu258@gmail.com>
2023-12-13 12:02:43 -0300

    Commit Inicial do Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import Equipe, DiasUteis, Frequencia
+
 # Register your models here.
+@admin.register(Equipe)
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome',
+        'vr_dia',
+        'vt_dia'
+    )
+
+@admin.register(DiasUteis)
+class DiasUteisAdmin(admin.ModelAdmin):
+    list_display = (
+        'ano_mes',
+        'qtd_du'
+    )
+
+
+@admin.register(Frequencia)
+class FrequenciaAdmin(admin.ModelAdmin):
+    list_display = (
+        'usuario',
+        'mes_referencia',
+        'qtd_home_office',
+        'qtd_faltas'
+    )
--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    criado = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Criação', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+    
+
+class Equipe(Base):
+    nome = models.CharField('Nome', max_length=100, null=False)
+    vr_dia = models.DecimalField('Vale Refeição',max_digits=5, decimal_places=2, default=45.00, null=False)
+    vt_dia = models.DecimalField('Vale Transporte', max_digits=5, decimal_places=2, null=False)
+
+    def __str__(self):
+        return self.nome
+    
+
+class DiasUteis(Base):
+    ano_mes = models.CharField('Ano - Mês', max_length=20)
+    qtd_du = models.IntegerField('Dias Úteis')
+
+    def __str__(self):
+        return self.ano_mes
+    
+
+class Frequencia(Base):
+    usuario = models.ForeignKey('core.Equipe', verbose_name='Usuário', on_delete=models.CASCADE)
+    mes_referencia = models.ForeignKey('core.DiasUteis', verbose_name='Mês de referência', on_delete=models.CASCADE)
+    qtd_home_office = models.IntegerField("Quantidade de Home Office")
+    qtd_faltas = models.IntegerField("Quantidade de Faltas")
+
+    def __str__(self):
+        return f'{self.ano_mes} - {self.mes_referencia}'
