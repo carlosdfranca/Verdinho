13a540bb9fb3d91114baac1bb1a3eacac1a68c1c
Carlos França <carlosdudu258@gmail.com>
2023-12-20 13:20:28 -0300

    Criando sessão do Usuário

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, UsuarioView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('usuario', UsuarioView.as_view(), name='usuario')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class UsuarioView(TemplateView):
+    template_name = 'usuario.html'
