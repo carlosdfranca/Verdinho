d6ac7aa6e6fa75d95534b86c6d9892b4d58d4acb
Carlos França <carlosdudu258@gmail.com>
2023-12-22 11:05:33 -0300

    testes de login sde usuário

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('calculadora/', CalculadoraView.as_view(), name='calculadora'),
-    path('logout/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('usuario/', User.as_view(), name='user')
 ]
