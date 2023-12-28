34becdf632b9d9e395c59359bc70170de344301a
Carlos Eduardo <carlosdudu258@gmail.com>
2023-12-28 09:42:45 -0300

    Revert "Tentativa de funcionar FALHAAA"
    This reverts commit 7817ff3c4ec0c7c195a990f16599244defc2d297.

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, CalculadoraView, User, dados_do_grafico
+from .views import IndexView, CalculadoraView, User
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -10,5 +10,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('usuario/', User.as_view(), name='user'),
-    path('dados_do_grafico/', dados_do_grafico, name='dados_do_grafico'),
 ]
